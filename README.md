347f5334162167db12b9f7d3c876b0f89206ab05
wgreene <wgreene@redhat.com>
2022-06-24 17:16:01 -0400

    First push with example scripts from https://github.com/confluentinc/examples

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # PeopleStream-for-LU-MSIT
 Final project for CSIS505 for a MSIT at Liberty University
+
+# RUN
+Set credentials for accessing the Kafka Cluster hosted by Confluent:
+    ```
+        export CONFLUENT_KAFKA_BROKER_USERNAME=redact
+        export CONFLUENT_KAFKA_BROKER_PASSWORD=redact
+    ```
+
+Run `run.sh`
+    ```
+        ./run.sh
+    ```
---
+++ run.sh
@@ -0,0 +1,2 @@
+CONFLUENT_KAFKA_BROKER_USERNAME="$CONFLUENT_KAFKA_BROKER_USERNAME" CONFLUENT_KAFKA_BROKER_PASSWORD="$CONFLUENT_KAFKA_BROKER_PASSWORD" envsubst < ./.confluent/java.config > ./.confluent/java.config.sensitive
+mvn exec:java -Dexec.mainClass="examples.ProducerExample" -Dexec.args="./.confluent/java.config.sensitive marketing-automation-platform--retriever"
